65805345e7749992ba7d0fd854f85e97317ff85f
Fakhrulhilal M <fakhrulhilal@gmail.com>
2024-02-06 00:44:44 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Playwright;
+using PlaywrightScript;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.LaunchEdge();
+var page = await browser.GetPageAsync();
+await page.CleanupWishlist();
